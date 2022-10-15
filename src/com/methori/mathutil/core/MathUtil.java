db35ed3c06cca9315bf850990d45ec55818c28df
srx <SeraphUltimateX@gmail.com>
2022-10-15 09:06:00 +0700

    update getFactorial() to use recursion

--- src/com/methori/mathutil/core/MathUtil.java
+++ src/com/methori/mathutil/core/MathUtil.java
@@ -17,15 +17,25 @@ public class MathUtil {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid input. Input must be between 0..20");
         }
-
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+//        if (n < 2) {
+//            return 1;
+//        }
+        return n * getFactorial(n - 1);
     }
+
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid input. Input must be between 0..20");
+//        }
+//
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
 }
